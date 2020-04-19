fd649433d5e94bd7047bf693e2151d8023d2db2f
marcelosvh <marcelosvh@hotmail.com>
2020-04-19 16:56:37 -0500

    first commit

---
+++ Covid19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19
+//
+//  Created by Marcelo Velasquez Herrera on 4/19/20.
+//  Copyright © 2020 Marcelo Stefano Velasquez Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
