2d60db18ac70ecc9a502e33c2c4c0108da680a95
marcelosvh <marcelosvh@hotmail.com>
2020-04-19 17:31:21 -0500

    Create Statistics Files

---
+++ Covid19/New Group/Statistics/View/Controller/StatisticsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StatisticsViewController.swift
+//  Covid19
+//
+//  Created by Marcelo Velasquez Herrera on 4/19/20.
+//  Copyright © 2020 Marcelo Stefano Velasquez Herrera. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
